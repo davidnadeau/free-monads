4ba452986fa2b2df3b93f09494aa883ad00aad10
davidnadeau <davidnadoh@gmail.com>
2016-11-08 00:04:47 -0500

    remove smart constructors

--- src/main/scala/Coproducts.scala
+++ src/main/scala/Coproducts.scala
@@ -1,88 +1,77 @@
 import cats.data.Coproduct
 import cats.free.Inject
 
+import scala.language.higherKinds
+
 object Coproducts extends App {
   import cats._
   import cats.free.Free
-  import algebra._, dsl._
+  import algebra._
+  import algebra.lift
 
 
   object algebra {
+    implicit def lift[F[_], G[_], A](fa: F[A])(implicit I: Inject[F, G]): Free[G, A] =
+      Free.liftF(I.inj(fa))
+
     sealed trait Calc[A]
-    case object Recall extends Calc[Int]
-    case class Increment(i: Int) extends Calc[Unit]
-    case object Clear extends Calc[Unit]
+    object Calc {
+      case object Recall extends Calc[Int]
+      case class Increment(i: Int) extends Calc[Unit]
+      case object Clear extends Calc[Unit]
+
+    }
 
     sealed trait Interact[A]
-    case class Ask(prompt: String) extends Interact[String]
-    case class Tell(msg: String) extends Interact[Unit]
+    object Interact {
+      case class Ask(prompt: String) extends Interact[String]
+      case class Tell(msg: String) extends Interact[Unit]
+    }
 
     case class User(id: String, password: String)
     sealed trait Auth[A]
-    case class Login(id: String, password: String) extends Auth[User]
-    case class HasPermission(user: User, permission: String) extends Auth[Boolean]
+    object Auth {
+      case class Login(id: String, password: String) extends Auth[User]
+      case class HasPermission(user: User, permission: String) extends Auth[Boolean]
+    }
 
     type M0[A] = Coproduct[Auth, Interact, A]
     type App[A] = Coproduct[Calc, M0, A]
-  }
-
-  object dsl {
-
-    class CalcActions[F[_]](implicit I: Inject[Calc, F]) {
-      def recall = Free.inject[Calc, F](Recall)
-      def increment(i: Int) = Free.inject[Calc, F](Increment(i))
-      def clear = Free.inject[Calc, F](Clear)
-    }
-    object CalcActions {
-      implicit def calcActions[F[_]](implicit I: Inject[Calc, F]): CalcActions[F] = new CalcActions[F]()
-    }
-
-    class InteractActions[F[_]](implicit I: Inject[Interact, F]) {
-      def ask(prompt: String) = Free.inject[Interact, F](Ask(prompt))
-      def tell(msg: String) = Free.inject[Interact, F](Tell(msg))
-    }
-
-    object InteractActions {
-      implicit def interactActions[F[_]](implicit I: Inject[Interact, F]): InteractActions[F] = new InteractActions[F]()
-    }
-
-    class AuthActions[F[_]](implicit I: Inject[Auth, F]) {
-      def login(id: String, password: String): Free[F, User] = Free.inject[Auth, F](Login(id, password))
-      def hasPermission(user: User, permission: String): Free[F, Boolean] = Free.inject[Auth, F](HasPermission(user, permission))
-    }
-    object AuthActions {
-      implicit def authActions[F[_]](implicit I: Inject[Auth, F]): AuthActions[F] = new AuthActions[F]()
-    }
 
   }
-  def calcProgram(implicit I: InteractActions[App], C: CalcActions[App]): Free[App, Int] = {
-    import I._, C._
+
+  def calcProgram[F[_]](implicit I: Inject[Interact, F], C: Inject[Calc, F]): Free[F, Int] = {
+    import Interact._
+    import Calc._
 
     for {
-      m0 <- recall
-      _ <- tell(s"starting at $m0")
-      n0 <- ask("give a number")
-      _ <- increment(n0.toInt)
-      m1 <- recall
-      i <- ask("continue?")
-      m1 <- if (i == "y") calcProgram else recall
+      m0 <- Recall
+      _ <- Tell(s"starting at $m0")
+      n0 <- Ask("give a number")
+      _ <- Increment(n0.toInt)
+      m1 <- Recall
+      i <- Ask("continue?")
+      m1 <- if (i == "y") calcProgram else Free.pure[F, Int](m1)
     } yield m1
-
   }
-  def guard(implicit A: AuthActions[App], I: InteractActions[App], C: CalcActions[App]): Free[App, Unit] = {
-    import I._, A._, C._
+
+  def guard[F[_]](implicit A: Inject[Auth, F], I: Inject[Interact, F], C: Inject[Calc, F]): Free[F, Unit] = {
+    import Interact._
+    import Auth._
 
     for {
-      id <- ask("username?")
-      pass <- ask("password?")
-      user <- login(id, pass)
-      b <- hasPermission(user, "")
-      m1 <- if (b) calcProgram else tell("can't use it")
+      id <- Ask("username?")
+      pass <- Ask("password?")
+      user <- Login(id, pass)
+      b <- HasPermission(user, "")
+      m1 <- if (b) calcProgram else Free.pure[F, String]("Can't use this program")
+      _ <- Tell(m1.toString)
     } yield ()
   }
 
   def calcInterpreter: Calc ~> Id =
     new (Calc ~> Id) {
+      import Calc._
       var memory = 0
       def apply[A](fa: Calc[A]) =
         fa match {
@@ -98,7 +87,7 @@ object Coproducts extends App {
     }
   def interactInterpreter: Interact ~> Id =
     new (Interact ~> Id) {
-
+      import Interact._
       def apply[A](fa: Interact[A]): Id[A] =
         fa match {
           case Ask(prompt) =>
@@ -110,7 +99,7 @@ object Coproducts extends App {
     }
   def authInterpreter: Auth ~> Id =
     new (Auth ~> Id) {
-
+      import Auth._
       def apply[A](fa: Auth[A]): Id[A] =
         fa match {
           case Login(id, password) => User(id, password).asInstanceOf[A]
@@ -121,6 +110,8 @@ object Coproducts extends App {
 
   val interpreter0: M0 ~> Id = authInterpreter or interactInterpreter
   val interpreter: App ~> Id = calcInterpreter or interpreter0
-  println(guard.foldMap(interpreter))
+  val program: Free[App, Unit] = guard[App]
+
+  println(program.foldMap(interpreter))
 }
 
